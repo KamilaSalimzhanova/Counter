3318bf959966a10d6ab89fc1eea61aea99ef9aa0
ksalimzh <ksalimzh@gmail.com>
2023-10-24 23:59:17 +0600

    Упрощенная версия 3 спринта дз

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var counterText: UILabel!
+    @IBOutlet weak var nameInBackground: UILabel!
+    
+    private var counter: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterText.text = "Значение счётчика: " + String(counter)
+        counterText.adjustsFontSizeToFitWidth = true
+        changeButton.setTitle("Увеличить", for: .normal)
+        changeButton.titleLabel?.font = .systemFont(ofSize: 30)
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        counterText.text = "Значение счётчика: " + String(counter)
+    }
 }
 
